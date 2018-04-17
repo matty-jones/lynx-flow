fc71d2e01c26fea6ac521c8948e526d23b22c58d
Matty Jones <mattyjones@boisestate.edu>
2018-04-17 17:09:14 -0600

    Set up the workspace to prevent duplication of lynx-create-morph jobs for HOOMD simulations of the same morphology but with different simulation parameters

--- create_workspace.py
+++ create_workspace.py
@@ -1,31 +1,48 @@
 import sys
 import signac
 import itertools
+import copy
 from collections import OrderedDict
 
 
-def get_parameters():
+def get_gen_parameters():
     parameters = OrderedDict()
     # Generate Parameters
     parameters['stoichiometry'] = ["{'Mo':1,'V':0.3,'Nb':0.15,'Te':0.15}"]
-    parameters['dimensions'] = ['10x10x1', '10x10x2', '10x10x3']
+    parameters['dimensions'] = ['10x10x1']
     parameters['template'] = ['M1UnitCell.pdb']
     parameters['crystal_separation'] = [25.0]
-    parameters['z_reactor_size'] = [10, 15, 20, 25]
+    parameters['z_reactor_size'] = [10, 15]
     parameters['gas_composition'] = ["{'C2H6':1}"]
     parameters['gas_density'] = [0.001356]
     parameters['forcefield'] = ['FF_opls_uff']
+    parameters['stage'] = ['parent']
+    return list(parameters.keys()),list(itertools.product(
+        *parameters.values()))
+
+
+def get_sim_parameters():
+    parameters = OrderedDict()
     # Simulate Parameters
-    parameters['temperature'] = [233, 333, 433, 533, 633, 733, 833]
+    parameters['temperature'] = [633, 733]
     parameters['run_time'] = [1E6]
     parameters['timestep'] = [1E-3]
+    parameters['stage'] = ['child']
     return list(parameters.keys()),list(itertools.product(
         *parameters.values()))
 
 
 if __name__ == "__main__":
     project = signac.init_project('FirstParSweep')
-    param_names, param_combinations = get_parameters()
-    for params in param_combinations:
-        job = project.open_job(dict(zip(param_names, params))).init()
+    gen_param_names, gen_param_combinations = get_gen_parameters()
+    sim_param_names, sim_param_combinations = get_sim_parameters()
+    # Create the generate jobs
+    for gen_params in gen_param_combinations:
+        parent_statepoint = dict(zip(gen_param_names, gen_params))
+        project.open_job(parent_statepoint).init()
+        for sim_params in sim_param_combinations:
+            child_statepoint = copy.deepcopy(parent_statepoint)
+            child_statepoint.update(dict(zip(sim_param_names, sim_params)))
+            child_statepoint['parent_statepoint'] = parent_statepoint
+            project.open_job(child_statepoint).init()
     project.write_statepoints()
