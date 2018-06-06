7080fb308fc93a5c00971e3d3c57c98eebd7232e
Matty Jones <mattyjones@boisestate.edu>
2018-06-06 16:41:09 -0600

    Added the job ID as an identifier when creating the view

--- create_view.py
+++ create_view.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
             job_list = project.find_job_ids({'dimensions': ''.join(['10x10x', str(layer)]),
                                              'z_reactor_size': z_size})
             view_name = ''.join(['layer', str(layer), 'z_size', str(z_size)])
-            project.create_linked_view(prefix = os.path.join(cwd, view_name),
+            project.create_linked_view(prefix = os.path.join(cwd, 'views',
+                                                             view_name),
                                        job_ids = job_list,
                                       )
