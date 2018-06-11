c76fb29e235849b87bccc64a92577ceb31b447ce
Matty Jones <mattyjones@boisestate.edu>
2018-06-11 15:08:35 -0600

    Blackened clear_document

--- analysis/clear_document.py
+++ analysis/clear_document.py
@@ -1,7 +1,7 @@
 import signac
 
 if __name__ == "__main__":
-    project = signac.get_project('../')
+    project = signac.get_project("../")
     for job in project:
         print(job.ws)
-        job.document.pop('RDF_first_peak')
+        job.document.pop("RDF_first_peak")
