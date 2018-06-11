792e18b614aa0dc1b8f08c6451ab08e1ea1aa51d
Matty Jones <mattyjones@boisestate.edu>
2018-06-11 15:07:47 -0600

    Incorporated the RDF crystal extremity changes and blackened the code

--- analysis/RDF_plots.py
+++ analysis/RDF_plots.py
@@ -7,11 +7,12 @@ import signac
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
 import numpy as np
+import argparse
 from scipy.signal import argrelextrema
 from scipy.ndimage import gaussian_filter
 
 
-'''
+"""
 This module plots several RDFs for each job in the workspace.
 10 RDFs are plotted over time as the simulation progresses (data taken from the
 trajectory GSD), as well as an aggregated RDF describing the average over the
@@ -19,87 +20,111 @@ entire simulation.
 Additionally, csv files are written for every RDF to permit subsequent analysis
 All 22 files (2 * 11) are written for each metal atom in the system (Mo, Nb,
 Te, V).
-'''
+"""
 
-def plot_z(project, type_name):
+
+def find_crystal_extents_z(project, type_name, args):
+    """
+    This function finds the z coordinates of the outermost layer of crystal,
+    i.e. the one that is exposed to the reactant and will actually undergo the
+    catalysed reaction.
+    This is important when calculating the RDFs to make sure that the features
+    do not get washed out as more crystal layers are included.
+    """
     for job in project:
+        if args.job is not None:
+            if job.get_id() != args.job:
+                continue
         print("Considering job", job.ws)
-        if int(job.statepoint.dimensions.split('x')[2]) in [1,2]:
-            continue
-        print(job.statepoint.dimensions)
-        print(job.statepoint)
         print("".join(["Calculating Z-distribution for ", type_name, "..."]))
-        save_dir = os.path.join(job.ws, 'RDFs')
+        # print(job.statepoint.dimensions)
+        # if job.statepoint.dimensions.split('x')[2] != '1':
+        #     continue
+        save_dir = os.path.join(job.ws, "RDFs")
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
-        gsd_file_name = os.path.join(job.ws, 'output_traj.gsd')
-        gsd_file = gsd.fl.GSDFile(gsd_file_name, 'rb')
+        gsd_file_name = os.path.join(job.ws, "output_traj.gsd")
+        gsd_file = gsd.fl.GSDFile(gsd_file_name, "rb")
         trajectory = gsd.hoomd.HOOMDTrajectory(gsd_file)
         atom_type = trajectory[0].particles.types.index(type_name)
         final_frame = trajectory[-1]
-        atom_posns = final_frame.particles.position[np.where(final_frame.particles.typeid == atom_type)]
+        atom_posns = final_frame.particles.position[
+            np.where(final_frame.particles.typeid == atom_type)
+        ]
         n_troughs = 0
-        # The number of peaks is 2x the z-dimension given.
-        # The number of troughs is therefore 2x the (z-dimension -1)
-        # Note that the middle bit is too flat to be detected so it usually isn't.
-        target_troughs = (int(job.statepoint.dimensions.split('x')[2]) - 1) * 2
-        # Start with a guess of 20 bins to try and find the right number of troughs
+        target_troughs = (int(job.statepoint.dimensions.split("x")[2]) - 1) * 2
         n_bins = 20
         bins = None
+        central_bins = None
         n = None
         while n_troughs < target_troughs:
             print("Currently found", n_troughs, "of", target_troughs, "troughs...")
-            n, bins, patches = plt.hist(atom_posns[:,2], bins = np.linspace(-job.statepoint.crystal_separation, job.statepoint.crystal_separation, n_bins))
-            # Recreate the bins so that the n value for each corresponds to the midpoint of the bin
-            bins = (bins[1:] + bins[:-1]) / 2.0
-            # Find all troughs
+            n, bins = np.histogram(
+                atom_posns[:, 2],
+                bins=np.linspace(
+                    -job.statepoint.crystal_separation,
+                    job.statepoint.crystal_separation,
+                    n_bins,
+                ),
+            )
+            # n, bins, patches = plt.hist(atom_posns[:,2], bins = np.linspace(-job.statepoint.crystal_separation, job.statepoint.crystal_separation, n_bins))
+            central_bins = (bins[1:] + bins[:-1]) / 2.0
             troughs = argrelextrema(n, np.less)[0]
             n_troughs = len(troughs)
-            # The Gaussian filter is just to help visualize what's going on with the histogram
             smoothed_n = gaussian_filter(n, 1.0)
-            plt.figure()
-            plt.title(" ".join(["Z-separation of", type_name]))
-            plt.plot(bins, n)
-            plt.plot(bins, smoothed_n, c='r')
-            for trough in troughs:
-                plt.axvline(bins[trough], c='k')
-            plt.xlabel('Z-separation (Ang)')
-            plt.ylabel('Frequency (Arb. U.)')
-            plt.show()
-            #plt.savefig(os.path.join(save_dir, av_rdf_title + '.pdf'))
-            plt.close()
-            # We haven't yet found the right number of troughs, therefore we need to step up the
-            # bin resolution, which we can do by doubling the current number of bins
-            print("Increasing n_bins from", n_bins, "to", n_bins*2)
+            # plt.figure()
+            # plt.title(" ".join(["Z-separation of", type_name]))
+            # plt.plot(central_bins, n)
+            # plt.plot(central_bins, smoothed_n, c='r')
+            # for trough in troughs:
+            #     plt.axvline(central_bins[trough], c='k')
+            # plt.xlabel('Z-separation (Ang)')
+            # plt.ylabel('Frequency (Arb. U.)')
+            # plt.show()
+            # #plt.savefig(os.path.join(save_dir, av_rdf_title + '.pdf'))
+            # plt.close()
+
+            print("Increasing n_bins from", n_bins, "to", n_bins * 2)
             n_bins *= 2
-        print("Found all", n_troughs, "troughs:")
-        print(troughs)
-        smoothed_n = gaussian_filter(n, 1.0)
-        plt.title(" ".join(["Z-separation of", type_name]))
-        plt.plot(bins[:-1], n)
-        plt.plot(bins[:-1], smoothed_n, c='r')
-        for trough in troughs:
-            plt.axvline(bins[trough], c='k')
-        plt.xlabel('Z-separation (Ang)')
-        plt.ylabel('Frequency (Arb. U.)')
-        plt.show()
-        #plt.savefig(os.path.join(save_dir, av_rdf_title + '.pdf'))
-        plt.close()
-        print("")
-        print("Trough positions =", [bins[x+1] for x in troughs])
-        exit()
+        if n_troughs == 0:
+            # Only one layer, so include everything
+            job.document["crystal_min_z"] = 0.0
+            job.document["crystal_max_z"] = 0.0
+        else:
+            print("Found all", n_troughs, "troughs:")
+            print(troughs)
+            # smoothed_n = gaussian_filter(n, 1.0)
+            # plt.title(" ".join(["Z-separation of", type_name]))
+            # plt.plot(central_bins, n)
+            # plt.plot(central_bins, smoothed_n, c='r')
+            # for trough in troughs:
+            #     plt.axvline(central_bins[trough], c='k')
+            # plt.xlabel('Z-separation (Ang)')
+            # plt.ylabel('Frequency (Arb. U.)')
+            # plt.show()
+            # #plt.savefig(os.path.join(save_dir, av_rdf_title + '.pdf'))
+            # plt.close()
+            print("")
+            trough_positions = [central_bins[x] for x in troughs]
+            print("Trough positions =", [central_bins[x] for x in troughs])
+            job.document["crystal_min_z"] = min(trough_positions)
+            job.document["crystal_max_z"] = max(trough_positions)
 
 
-def plot_rdf(project, type1_name, type2_name, r_max=20, stride=50):
+def plot_rdf(project, type1_name, type2_name, args, r_max=20, stride=50):
     for job in project:
+        if args.job is not None:
+            if job.get_id() != args.job:
+                continue
         print("Considering job", job.ws)
-        print("".join(["Calculating RDFs between ", type1_name, "-",
-                       type2_name, "..."]))
-        save_dir = os.path.join(job.ws, 'RDFs')
+        print(
+            "".join(["Calculating RDFs between ", type1_name, "-", type2_name, "..."])
+        )
+        save_dir = os.path.join(job.ws, "RDFs")
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
-        gsd_file_name = os.path.join(job.ws, 'output_traj.gsd')
-        gsd_file = gsd.fl.GSDFile(gsd_file_name, 'rb')
+        gsd_file_name = os.path.join(job.ws, "output_traj.gsd")
+        gsd_file = gsd.fl.GSDFile(gsd_file_name, "rb")
         trajectory = gsd.hoomd.HOOMDTrajectory(gsd_file)
         sim_box = trajectory[0].configuration.box[:3]
         av_rdf = freud.density.RDF(rmax=r_max, dr=0.1)
@@ -107,60 +132,108 @@ def plot_rdf(project, type1_name, type2_name, r_max=20, stride=50):
         type1 = trajectory[0].particles.types.index(type1_name)
         type2 = trajectory[0].particles.types.index(type2_name)
         for frame_no, frame in enumerate(trajectory):
-            print("".join(["\rCalculating RDF for frame ", frame_no + 1,
-                           " of ", len(trajectory)]), end=' ')
+            print(
+                "".join(
+                    [
+                        "\rCalculating RDF for frame ",
+                        str(frame_no + 1),
+                        " of ",
+                        str(len(trajectory)),
+                    ]
+                ),
+                end=" ",
+            )
             frame_rdf = freud.density.RDF(rmax=r_max, dr=0.1)
             frame_rdf.resetRDF()
             # In case box size changes
             box = frame.configuration.box
             freud_box = freud.box.Box(Lx=box[0], Ly=box[1], Lz=box[2])
             type1_pos = frame.particles.position[
-                np.where(frame.particles.typeid == type1)]
+                np.where(frame.particles.typeid == type1)
+            ]
+            # old_type2_pos = frame.particles.position[
+            #     np.where(frame.particles.typeid == type2)]
+            # New Type 2 pos needs to be all particles of type type2,
+            # but with min_z <= z-position < max_z
             type2_pos = frame.particles.position[
-                np.where(frame.particles.typeid == type2)]
+                np.where(
+                    (frame.particles.typeid == type2)
+                    & (
+                        (
+                            frame.particles.position[:, 2]
+                            >= job.document["crystal_max_z"]
+                        )
+                        | (
+                            frame.particles.position[:, 2]
+                            <= job.document["crystal_min_z"]
+                        )
+                    )
+                )
+            ]
             av_rdf.accumulate(freud_box, type1_pos, type2_pos)
             if frame_no % stride == 0:
-                print("".join(["\rSaving RDF image for frame ", frame_no + 1,
-                               " of ", len(trajectory)]), end=' ')
+                print(
+                    "".join(
+                        [
+                            "\rSaving RDF image for frame ",
+                            str(frame_no + 1),
+                            " of ",
+                            str(len(trajectory)),
+                        ]
+                    ),
+                    end=" ",
+                )
                 frame_rdf.compute(freud_box, type1_pos, type2_pos)
-                frame_rdf_title = ''.join(['RDF_', type1_name, '-', type2_name,
-                                           '_{:03d}'.format(frame_no)])
-                with open(os.path.join(save_dir, frame_rdf_title + '.csv'),
-                          "w+") as csv_file:
+                frame_rdf_title = "".join(
+                    ["RDF_", type1_name, "-", type2_name, "_{:03d}".format(frame_no)]
+                )
+                with open(
+                    os.path.join(save_dir, frame_rdf_title + ".csv"), "w+"
+                ) as csv_file:
                     writer = csv.writer(csv_file)
-                    writer.writerow(['r', 'g(r)'])
+                    writer.writerow(["r", "g(r)"])
                     for r, g_r in zip(frame_rdf.getR(), frame_rdf.getRDF()):
                         writer.writerow([r, g_r])
                 plt.figure()
                 plt.title(frame_rdf_title)
                 plt.plot(frame_rdf.getR(), frame_rdf.getRDF())
-                plt.xlabel('r (Ang)')
-                plt.ylabel('RDF (Arb. U.)')
-                plt.savefig(os.path.join(save_dir, frame_rdf_title + '.pdf'))
+                plt.xlabel("r (Ang)")
+                plt.ylabel("RDF (Arb. U.)")
+                plt.savefig(os.path.join(save_dir, frame_rdf_title + ".pdf"))
                 plt.close()
-        print("\rCalculating RDF averaged over all frames", end=' ')
-        av_rdf_title = 'RDF_' + type1_name + '-' + type2_name + '_Av'
-        with open(os.path.join(save_dir, av_rdf_title + '.csv'),
-                  "w+") as csv_file:
+        print("\rCalculating RDF averaged over all frames", end=" ")
+        av_rdf_title = "RDF_" + type1_name + "-" + type2_name + "_Av"
+        with open(os.path.join(save_dir, av_rdf_title + ".csv"), "w+") as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow(['r', 'g(r)'])
+            writer.writerow(["r", "g(r)"])
             for r, g_r in zip(av_rdf.getR(), av_rdf.getRDF()):
                 writer.writerow([r, g_r])
         plt.figure()
         plt.title(av_rdf_title)
         plt.plot(av_rdf.getR(), av_rdf.getRDF())
-        plt.xlabel('r (Ang)')
-        plt.ylabel('RDF (Arb. U.)')
-        plt.savefig(os.path.join(save_dir, av_rdf_title + '.pdf'))
+        plt.xlabel("r (Ang)")
+        plt.ylabel("RDF (Arb. U.)")
+        plt.savefig(os.path.join(save_dir, av_rdf_title + ".pdf"))
         plt.close()
         print("")
 
 
 if __name__ == "__main__":
-    project = signac.get_project('../')
-    for type2 in ['Mo', 'V', 'Nb', 'Te']:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-j",
+        "--job",
+        type=str,
+        required=False,
+        default=None,
+        help=(
+            "If present, only consider the job in the current" " directory's workspace."
+        ),
+    )
+    args, directory_list = parser.parse_known_args()
+    project = signac.get_project("../")
+    for type2 in ["Mo", "V", "Nb", "Te"]:
         # Plot distribution of z-values for atoms
-        plot_z(project, type2)
-        exit()
+        find_crystal_extents_z(project, type2, args)
         # Plot RDF variation
-        plot_rdf(project, 'C', type2)
+        plot_rdf(project, "C", type2, args)
