c22db0a4c854b5bb42f6002c520cbe7ae25dccda
Matty Jones <mattyjones@boisestate.edu>
2018-06-11 16:59:44 -0600

    Blackened create_workspace

--- create_workspace.py
+++ create_workspace.py
@@ -8,32 +8,30 @@ from collections import OrderedDict
 def get_gen_parameters():
     parameters = OrderedDict()
     # Generate Parameters
-    parameters['stoichiometry'] = ["{'Mo':1,'V':0.3,'Nb':0.15,'Te':0.15}"]
-    parameters['dimensions'] = ['10x10x1']
-    parameters['template'] = ['M1UnitCell.pdb']
-    parameters['crystal_separation'] = [25.0]
-    parameters['z_reactor_size'] = [10, 15]
-    parameters['gas_composition'] = ["{'C2H6':1}"]
-    parameters['gas_density'] = [0.001356]
-    parameters['forcefield'] = ['FF_opls_uff']
-    parameters['job_type'] = ['parent']
-    return list(parameters.keys()),list(itertools.product(
-        *parameters.values()))
+    parameters["stoichiometry"] = ["{'Mo':1,'V':0.3,'Nb':0.15,'Te':0.15}"]
+    parameters["dimensions"] = ["10x10x1"]
+    parameters["template"] = ["M1UnitCell.pdb"]
+    parameters["crystal_separation"] = [25.0]
+    parameters["z_reactor_size"] = [10, 15]
+    parameters["gas_composition"] = ["{'C2H6':1}"]
+    parameters["gas_density"] = [0.001356]
+    parameters["forcefield"] = ["FF_opls_uff"]
+    parameters["job_type"] = ["parent"]
+    return list(parameters.keys()), list(itertools.product(*parameters.values()))
 
 
 def get_sim_parameters():
     parameters = OrderedDict()
     # Simulate Parameters
-    parameters['temperature'] = [633, 733]
-    parameters['run_time'] = [1E2]
-    parameters['timestep'] = [1E-3]
-    parameters['job_type'] = ['child']
-    return list(parameters.keys()),list(itertools.product(
-        *parameters.values()))
+    parameters["temperature"] = [633, 733]
+    parameters["run_time"] = [1E2]
+    parameters["timestep"] = [1E-3]
+    parameters["job_type"] = ["child"]
+    return list(parameters.keys()), list(itertools.product(*parameters.values()))
 
 
 if __name__ == "__main__":
-    project = signac.init_project('FirstParSweep')
+    project = signac.init_project("FirstParSweep")
     gen_param_names, gen_param_combinations = get_gen_parameters()
     sim_param_names, sim_param_combinations = get_sim_parameters()
     # Create the generate jobs
@@ -44,6 +42,6 @@ if __name__ == "__main__":
         for sim_params in sim_param_combinations:
             child_statepoint = copy.deepcopy(parent_statepoint)
             child_statepoint.update(dict(zip(sim_param_names, sim_params)))
-            child_statepoint['parent_statepoint'] = parent_job.statepoint()
+            child_statepoint["parent_statepoint"] = parent_job.statepoint()
             project.open_job(child_statepoint).init()
     project.write_statepoints()
