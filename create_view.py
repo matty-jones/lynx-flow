0d75fc83706cd801fe0a3ec42d4fdac61fcbeeb1
Matty Jones <mattyjones@boisestate.edu>
2018-06-11 16:59:29 -0600

    Blackened create_view

--- create_view.py
+++ create_view.py
@@ -3,13 +3,16 @@ import os
 
 if __name__ == "__main__":
     cwd = os.getcwd()
-    project = signac.get_project('./')
+    project = signac.get_project("./")
     for layer in [1, 2, 3]:
         for z_size in [10.0, 15.0, 20.0, 25.0]:
-            job_list = project.find_job_ids({'dimensions': ''.join(['10x10x', str(layer)]),
-                                             'z_reactor_size': z_size})
-            view_name = ''.join(['layer', str(layer), 'z_size', str(z_size)])
-            project.create_linked_view(prefix = os.path.join(cwd, 'views',
-                                                             view_name),
-                                       job_ids = job_list,
-                                      )
+            job_list = project.find_job_ids(
+                {
+                    "dimensions": "".join(["10x10x", str(layer)]),
+                    "z_reactor_size": z_size,
+                }
+            )
+            view_name = "".join(["layer", str(layer), "z_size", str(z_size)])
+            project.create_linked_view(
+                prefix=os.path.join(cwd, "views", view_name), job_ids=job_list
+            )
