46eb720968109bfa195d02635e00f8caf16ac3eb
Matthew Jones <mattyjones@boisestate.edu>
2018-08-17 10:19:01 -0600

    Updated RDF_plots to use the COM of the organic molecules, if specified. Fixes #1.

--- analysis/RDF_plots.py
+++ analysis/RDF_plots.py
@@ -114,25 +114,149 @@ def find_crystal_extents_z(project, type_name, args):
             job.document["crystal_max_z"] = max(trough_positions)
 
 
-def update_bond_dict(bond_dict, frame, type_name):
-    bond_type = "-".join([type_name, type_name])
-    type_index = frame.bonds.types.index(bond_type)
-    bonded_atoms = frame.bonds.group[np.where(frame.bonds.typeid == type_index)]
-    for bond in bonded_atoms:
-        if (bond[0] < bond[1]):
-            if (bond[0] not in bond_dict):
-                bond_dict[bond[0]] = [bond[1]]
+def calc_COM(list_of_positions, list_of_atom_types=None, list_of_atom_masses=None):
+    """
+    This function calculates the centre of mass of a collection of sites/atoms
+    (listOfPositions) with corresponding type (listOfAtomTypes) or mass (listOfMasses)
+    if list_of_atom_masses is not specified, then list_of_atom_types must be.
+    """
+    if len(list_of_positions) == 1:
+        return np.array(list_of_positions[0])
+    mass_weighted = np.array([0.0, 0.0, 0.0])
+    if list_of_atom_masses is None:
+        list_of_atom_masses = []
+        for atom_type in list_of_atom_types:
+            # Masses obtained from nist.gov, for the atoms we are likely to
+            # simulate the most.
+            # Add in new atoms here if your molecule requires it!
+            if atom_type.lower()[:2] == "si":
+                list_of_atom_masses.append(27.976926)
+            elif atom_type.lower()[0] == "c":
+                list_of_atom_masses.append(12.000000)
+            elif atom_type.lower()[0] == "h":
+                list_of_atom_masses.append(1.0078250)
+            elif atom_type.lower()[0] == "s":
+                list_of_atom_masses.append(31.972071)
+            elif atom_type.lower()[0] == "o":
+                list_of_atom_masses.append(15.994914)
+            elif atom_type.lower()[0] == "n":
+                list_of_atom_masses.append(14.003074)
+            elif atom_type.lower()[:2] == "mo":
+                list_of_atom_masses.append(95.960000)
+            elif atom_type.lower()[:2] == "nb":
+                list_of_atom_masses.append(92.906380)
+            elif atom_type.lower()[:2] == "te":
+                list_of_atom_masses.append(127.60000)
+            elif atom_type.lower()[:2] == "v":
+                list_of_atom_masses.append(50.941500)
+            elif atom_type.lower()[:2] == "ni":
+                list_of_atom_masses.append(140.91120)
+            elif atom_type.lower()[:2] == "ga":
+                list_of_atom_masses.append(69.723000)
+            elif atom_type.lower()[:2] == "mn":
+                list_of_atom_masses.append(54.938045)
+            elif atom_type.lower()[:2] == "cu":
+                list_of_atom_masses.append(63.546000)
+            elif atom_type.lower()[:2] == "ag":
+                list_of_atom_masses.append(107.86820)
+            elif atom_type.lower()[:2] == "au":
+                list_of_atom_masses.append(196.96657)
+            elif (atom_type.lower()[0] == "d") or (atom_type.lower()[0] == "a"):
+                list_of_atom_masses.append(1.0)
             else:
-                bond_dict[bond[0]].append(bond[1])
+                raise SystemError(
+                    "Unknown atomic mass " + str(atom_type) + ". Please hardcode"
+                    " into helper_functions.calc_COM."
+                )
+    total_mass = np.sum(list_of_atom_masses)
+    for atom_ID, position in enumerate(list_of_positions):
+        for axis in range(3):
+            mass_weighted[axis] += position[axis] * list_of_atom_masses[atom_ID]
+    return mass_weighted / float(total_mass)
+
+
+def obtain_bonded_list(bond_list):
+    # Create a lookup table `neighbour list' for all connected atoms called
+    # {bondedAtoms}
+    bonded_atoms = {}
+    for bond in bond_list:
+        if bond[0] not in bonded_atoms:
+            bonded_atoms[bond[0]] = [bond[1]]
         else:
-            if (bond[1] not in bond_dict):
-                bond_dict[bond[1]] = [bond[0]]
-            else:
-                bond_dict[bond[1]].append(bond[0])
-    return bond_dict
+            bonded_atoms[bond[0]].append(bond[1])
+        if bond[1] not in bonded_atoms:
+            bonded_atoms[bond[1]] = [bond[0]]
+        else:
+            bonded_atoms[bond[1]].append(bond[0])
+    return bonded_atoms
 
 
-def plot_rdf(project, type1_name, type2_name, args, r_max=20, stride=50, all_type1_in_mol=False, all_type2_in_mol=False):
+def split_molecules(frame):
+    # Split the full morphology into individual molecules
+    # Create a lookup table `neighbour list' for all connected atoms called
+    # {bondedAtoms}
+    bonded_atoms = obtain_bonded_list(frame.bonds.group)
+    molecule_list = [i for i in range(len(frame.particles.typeid))]
+    # Recursively add all atoms in the neighbour list to this molecule
+    for mol_ID in range(len(molecule_list)):
+        molecule_list = update_molecule(mol_ID, molecule_list, bonded_atoms)
+    # Here we have a list of len(atoms) where each index gives the molID
+    mol_ID_dict = {}
+    for atom_ID, mol_ID in enumerate(molecule_list):
+        if mol_ID not in mol_ID_dict:
+            mol_ID_dict[mol_ID] = [atom_ID]
+        else:
+            mol_ID_dict[mol_ID].append(atom_ID)
+    return molecule_list, mol_ID_dict
+
+
+def update_molecule(atom_ID, molecule_list, bonded_atoms):
+    # Recursively add all neighbours of atom number atomID to this molecule
+    try:
+        for bonded_atom in bonded_atoms[atom_ID]:
+            # If the moleculeID of the bonded atom is larger than that of the
+            # current one, update the bonded atom's ID to the current one's to
+            # put it in this molecule, then iterate through all of the bonded
+            # atom's neighbours
+            if molecule_list[bonded_atom] > molecule_list[atom_ID]:
+                molecule_list[bonded_atom] = molecule_list[atom_ID]
+                molecule_list = update_molecule(
+                    bonded_atom, molecule_list, bonded_atoms
+                )
+            # If the moleculeID of the current atom is larger than that of the
+            # bonded one, update the current atom's ID to the bonded one's to
+            # put it in this molecule, then iterate through all of the current
+            # atom's neighbours
+            elif molecule_list[bonded_atom] < molecule_list[atom_ID]:
+                molecule_list[atom_ID] = molecule_list[bonded_atom]
+                molecule_list = update_molecule(atom_ID, molecule_list, bonded_atoms)
+            # Else: both the current and the bonded atom are already known to
+            # be in this molecule, so we don't have to do anything else.
+    except KeyError:
+        # This means that there are no bonded CG sites (i.e. it's a single molecule)
+        pass
+    return molecule_list
+
+
+def get_type_positions(AAID_list, frame, crystal_min_z=None, crystal_max_z=None):
+    type_positions = []
+    for AAID_sublist in AAID_list:
+        sublist_positions = []
+        sublist_types = []
+        for AAID in AAID_sublist:
+            position = frame.particles.position[AAID]
+            if (crystal_min_z is not None) and (crystal_max_z is not None):
+                if (position[2] < crystal_max_z) and (position[2] > crystal_min_z):
+                    continue
+            sublist_positions.append(position)
+            type_ID = frame.particles.typeid[AAID]
+            atom_type = frame.particles.types[type_ID]
+            sublist_types.append(atom_type)
+        type_positions.append(calc_COM(sublist_positions, list_of_atom_types=sublist_types))
+    return np.array(type_positions)
+
+
+def plot_rdf(project, type1_name, type2_name, args, r_max=20, stride=50, type1_by_mol=False, type2_by_mol=False):
     for job in project:
         if args.job is not None:
             if job.get_id() != args.job:
@@ -154,15 +278,41 @@ def plot_rdf(project, type1_name, type2_name, args, r_max=20, stride=50, all_typ
         sim_box = trajectory[0].configuration.box[:3]
         av_rdf = freud.density.RDF(rmax=r_max, dr=0.1)
         av_rdf.resetRDF()
-        type1 = trajectory[0].particles.types.index(type1_name)
-        type2 = trajectory[0].particles.types.index(type2_name)
-        important_bonds = {}
-        if all_type1_in_mol:
-            important_bonds = update_bond_dict(important_bonds, trajectory[0], type1_name)
-        if all_type2_in_mol:
-            important_bonds = update_bond_dict(important_bonds, trajectory[0], type1_name)
-        print(important_bonds)
-        exit()
+        type1_ID = trajectory[0].particles.types.index(type1_name)
+        type2_ID = trajectory[0].particles.types.index(type2_name)
+
+        AAID_to_molID, molID_to_AAIDs = split_molecules(trajectory[0])
+
+        if type1_by_mol is True:
+            # Return AAIDs for all atoms in the same molecule, if that molecule includes
+            # a type1_ID atom
+            type1_AAIDs = []
+            mols_considered = []
+            for AAID, molID in enumerate(AAID_to_molID):
+                if trajectory[0].particles.typeid[AAID] == type1_ID:
+                    if molID in mols_considered:
+                        continue
+                    mols_considered.append(molID)
+                    AAIDs_in_mol = molID_to_AAIDs[molID]
+                    type1_AAIDs.append(AAIDs_in_mol)
+        else:
+            type1_AAIDs = [[AAID] for AAID in np.where(trajectory[0].particles.typeid == type1_ID)[0]]
+
+        if type2_by_mol is True:
+            # Return AAIDs for all atoms in the same molecule, if that molecule includes
+            # a type1_ID atom
+            type2_AAIDs = []
+            mols_considered = []
+            for AAID, molID in enumerate(AAID_to_molID):
+                if trajectory[0].particles.typeid[AAID] == type2_ID:
+                    if molID in mols_considered:
+                        continue
+                    mols_considered.append(molID)
+                    AAIDs_in_mol = molID_to_AAIDs[molID]
+                    type2_AAIDs.append(AAIDs_in_mol)
+        else:
+            type2_AAIDs = [[AAID] for AAID in np.where(trajectory[0].particles.typeid == type2_ID)[0]]
+
         for frame_no, frame in enumerate(trajectory):
             print(
                 "".join(
@@ -181,41 +331,8 @@ def plot_rdf(project, type1_name, type2_name, args, r_max=20, stride=50, all_typ
             box = frame.configuration.box
             freud_box = freud.box.Box(Lx=box[0], Ly=box[1], Lz=box[2])
 
-
-            print("DEBUG")
-            important_type_ID = "-".join([type1_name, type1_name])
-            print(type(frame))
-            print(dir(frame.bonds))
-            print(frame.bonds.types)
-            print(frame.bonds.typeid)
-            print(frame.bonds.group)
-            print(frame.bonds.N)
-            exit()
-            
-            
-            
-            type1_pos = frame.particles.position[
-                np.where(frame.particles.typeid == type1)
-            ]
-            # old_type2_pos = frame.particles.position[
-            #     np.where(frame.particles.typeid == type2)]
-            # New Type 2 pos needs to be all particles of type type2,
-            # but with min_z <= z-position < max_z
-            type2_pos = frame.particles.position[
-                np.where(
-                    (frame.particles.typeid == type2)
-                    & (
-                        (
-                            frame.particles.position[:, 2]
-                            >= job.document["crystal_max_z"]
-                        )
-                        | (
-                            frame.particles.position[:, 2]
-                            <= job.document["crystal_min_z"]
-                        )
-                    )
-                )
-            ]
+            type1_pos = get_type_positions(type1_AAIDs, frame)
+            type2_pos = get_type_positions(type2_AAIDs, frame, crystal_min_z=job.document["crystal_min_z"], crystal_max_z=job.document["crystal_max_z"])
             av_rdf.accumulate(freud_box, type1_pos, type2_pos)
             if frame_no % stride == 0:
                 print(
@@ -282,4 +399,4 @@ if __name__ == "__main__":
         # Plot distribution of z-values for atoms
         find_crystal_extents_z(project, type2, args)
         # Plot RDF variation
-        plot_rdf(project, "C", type2, args, all_type1_in_mol=True)
+        plot_rdf(project, "C", type2, args, type1_by_mol=True)
