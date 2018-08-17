12ff8f857b6a7d50ae7f0cf09cda97f6fee177e6
Matthew Jones <mattyjones@boisestate.edu>
2018-08-17 11:50:49 -0600

    Generalized the RDF_progression_plots function to all surfaces

--- analysis/RDF_progression_plots.py
+++ analysis/RDF_progression_plots.py
@@ -16,14 +16,17 @@ def plot_first_peaks(project):
     colours = pl.cm.seismic(np.linspace(0, 1, 4))
     all_dimensions = []
     all_z_sizes = []
+    all_surface_atom_types = []
     # Iterate through all jobs in the project to find the unique dimensions
     # and reactor sizes
     for job in project:
         statepoint = job.sp()
         all_dimensions.append(statepoint["dimensions"])
         all_z_sizes.append(statepoint["z_reactor_size"])
+        all_surface_atom_types += list(eval(statepoint["stoichiometry"]).keys())
     all_dimensions = list(set(all_dimensions))
     all_z_sizes = list(set(all_z_sizes))
+    all_surface_atom_types = list(set(all_surface_atom_types))
 
     for plot_index, plot_type in enumerate(["Position (A)", "Magnitude (Arb. U.)"]):
         print(" ".join(["Creating", plot_type[:3].lower(), "plots..."]))
@@ -34,7 +37,7 @@ def plot_first_peaks(project):
             for z_size in all_z_sizes:
                 # New line on the plot for each z_size
                 print("Creating plots for reactor size =", z_size)
-                for index, metal in enumerate(["Mo", "Nb", "Te", "V"]):
+                for index, atom_type in enumerate(all_surface_atom_types):
                     temperatures = []
                     first_peak_data = []
                     for job in project.find_jobs(
@@ -45,14 +48,14 @@ def plot_first_peaks(project):
                     ):
                         try:
                             first_peak = job.document[
-                                "_".join(["RDF_first_peak", metal])
+                                "_".join(["RDF_first_peak", atom_type])
                             ]
                             first_peak_data.append(first_peak[plot_index])
                             temperatures.append(job.sp()["temperature"])
                         except KeyError:
                             continue
                     plot_T, plot_peak = zip(*sorted(zip(temperatures, first_peak_data)))
-                    plt.plot(plot_T, plot_peak, color=colours[index], label=metal)
+                    plt.plot(plot_T, plot_peak, color=colours[index], label=atom_type)
                 plt.xlabel("T (K)")
                 plt.ylabel(plot_type)
                 plt.title("".join(["Dims = ", dimension, ", Z = ", str(z_size)]))
