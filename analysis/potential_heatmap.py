590cc6106256636bae81ec731e5bc31e5f1df440
Matthew Jones <mattyjones@boisestate.edu>
2018-10-03 17:07:04 -0600

    Heatmap is now created and viewable and is pretty fast. Now to work on GUI

--- analysis/potential_heatmap.py
+++ analysis/potential_heatmap.py
@@ -1,18 +1,16 @@
-import csv
 import gsd.fl
 import gsd.hoomd
 import os
 import signac
+import matplotlib.colors
+import matplotlib.cm as cmx
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
 import numpy as np
 import argparse
-import itertools
-import operator
-from scipy.signal import argrelextrema
-from scipy.ndimage import gaussian_filter
-from rhaco.simulate import AVOGADRO, BOLTZMANN, KCAL_TO_J, AMU_TO_KG, ANG_TO_M
-
+import freud.locality
+import freud.box
+import rhaco.simulate
 
 """
 This module plots the residence time distributions for each job in the workspace.
@@ -49,15 +47,23 @@ def get_surface_atoms(job, morphology):
         float(job.sp["crystal_z"]) * 10.0 * float(job.sp["dimensions"].split("x")[2])
     )
     z_vals = morphology.particles.position[:,2]
-    return morphology.particles.position[(z_vals > z_min) * (z_vals < z_max)]
+    surface_posns = morphology.particles.position[(z_vals > z_min) * (z_vals < z_max)]
+    surface_type_ID = morphology.particles.typeid[(z_vals > z_min) * (z_vals < z_max)]
+    type_lookup = {ID: type_name for ID, type_name in enumerate(morphology.particles.types)}
+    surface_type = np.vectorize(type_lookup.get)(surface_type_ID)
+    return surface_posns, surface_type
 
 
-def create_mesh(morphology, lattice_spacing, z_position):
+def create_mesh(morphology, args):
+    lattice_spacing = args.mesh_spacing
+    mesh_type = args.mesh_type
+    z_position = args.z_val
     box_dims = morphology.configuration.box[:3]
     # Work out all of the x, y, and z values
     x_vals = np.arange(-box_dims[0]/2.0, box_dims[0]/2.0, lattice_spacing)
     y_vals = np.arange(-box_dims[1]/2.0, box_dims[1]/2.0, lattice_spacing)
     z_vals = np.array([z_position])
+    mesh_shape = (len(x_vals), len(y_vals))
     # Create the mesh coordinates using vstack and meshgrid
     mesh_coords = np.vstack(np.meshgrid(x_vals, y_vals, z_vals)).reshape(3, -1).T
     # Now centre the mesh over the middle
@@ -74,7 +80,103 @@ def create_mesh(morphology, lattice_spacing, z_position):
         ]
         + [0.0]
     )
-    return mesh_coords
+    mesh_types = [mesh_type] * len(mesh_coords)
+    return mesh_coords, mesh_types, mesh_shape
+
+
+def create_freud_nlist(job_frame, crystal_mesh_posns, mesh_size, cut_off):
+    # Firstly create the simulation box so that Freud can do the periodic stuff
+    simulation_box = freud.box.Box(*job_frame.configuration.box)
+    # Calculate the cell list based on the input cut_off (same as used in the sim)
+    cell_list = freud.locality.LinkCell(simulation_box, cut_off)
+    # Compute the neighbourlist for all particles in the crystal and mesh
+    cell_list.compute(simulation_box, crystal_mesh_posns)
+    neighbour_list = cell_list.nlist
+    # Create a neighbourlist dictionary of crystal_IDs for each probe atom in the mesh
+    nlist = {}
+    for probe_ID in range(mesh_size):
+        crystal_IDs = neighbour_list.index_j[neighbour_list.index_i == probe_ID]
+        nlist[probe_ID] = [int(ID) for ID in crystal_IDs if ID >= mesh_size]
+    return nlist
+
+
+def calculate_potentials(
+    job, nlist, n_probes, crystal_mesh_posns, crystal_mesh_types, u_max
+):
+    ff_coeffs = rhaco.simulate.get_coeffs(os.path.join(job.ws, "output.hoomdxml"))
+    pair_coeffs = {coeff[0]: [coeff[1], coeff[2]] for coeff in ff_coeffs["pair_coeffs"]}
+    if len(ff_coeffs["external_forcefields"]) > 0:
+        raise SystemError("EXTERNAL FORCEFIELD DETECTED (E.G. EAM), CANNOT INTERPRET")
+    probe_atoms = {}
+    for probe_index in range(n_probes):
+        probe_atoms[probe_index] = [crystal_mesh_posns[probe_index]]
+    # NOTE If this is slow, then consider vectorizing it by passing in all neighbours
+    # at once
+    for probe_ID, neighbours in nlist.items():
+        potential = 0.0
+        type_1 = crystal_mesh_types[probe_ID]
+        pos_1 = crystal_mesh_posns[probe_ID]
+        for neighbour_ID in neighbours:
+            type_2 = crystal_mesh_types[neighbour_ID]
+            pos_2 = crystal_mesh_posns[neighbour_ID]
+            potential += LJ_pair_potential(pair_coeffs, type_1, type_2, pos_1, pos_2)
+        # If the potential is more than about 5 then there's no chance of the particle
+        # being there, so set this as a cap
+        if (u_max is not None) and (potential > u_max):
+            potential = u_max
+        probe_atoms[probe_ID].append(potential)
+    return probe_atoms
+
+
+def LJ_pair_potential(coeffs, type1, type2, type1_posn, type2_posn):
+    # Using the geometric mixing rules
+    epsilon = np.sqrt(coeffs[type1][0] * coeffs[type2][0])
+    sigma = np.sqrt(coeffs[type1][1] * coeffs[type2][1])
+    r = np.sqrt(np.sum((np.array(type1_posn) - np.array(type2_posn)) ** 2))
+    # Lennard-Jones 12-6 formula
+    potential = 4 * epsilon * ((sigma / r) ** 12 - (sigma / r) ** 6)
+    return potential
+
+
+def create_potential_array(potential_dict, mesh_shape, args):
+    potential_array = np.zeros(mesh_shape)
+    lattice_spacing = args.mesh_spacing
+    # Obtain the mesh_positions
+    mesh_details = np.array([np.append(val[0], [val[1]]) for val in potential_dict.values()])
+    # We're not going to need the z values so we can drop those here.
+    mesh_posns = mesh_details[:,[0,1]]
+    mesh_potentials = mesh_details[:,3]
+    # Since we are no longer operating in coordinate space, shift the positions
+    # so that the bottom left (-large_x, -large_y) is now the origin
+    offset = np.array([np.min(mesh_posns[:,axis]) for axis in range(2)])
+    mesh_posns -= offset
+    # Divide by the lattice spacing
+    mesh_posns /= lattice_spacing
+    # Drop the z_coordinate as we don't need it, and remap the matrix as integers
+    mesh_posns = np.array(mesh_posns[:,[0,1]], dtype=int)
+    for probe_ID, probe_posn in enumerate(mesh_posns):
+        potential_array[probe_posn[0], probe_posn[1]] = mesh_potentials[probe_ID]
+    # As our origin is currently the bottom left (-large_x, -large_y), but array
+    # plotting subroutines have the origin in the top left, we need to flip the array
+    potential_array = np.flip(potential_array, 0)
+    return potential_array
+
+
+def plot_heatmap(input_array):
+    plt.clf()
+    # Reverse the colour map so that the `hot spots' show places where particles are
+    # more likely to reside (i.e. lower potential energy)
+    colour_map = plt.get_cmap("inferno_r")
+    c_norm = matplotlib.colors.Normalize(
+        vmin=np.min(input_array),
+        vmax=np.max(input_array),
+    )
+    scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=colour_map)
+    plt.imshow(input_array, cmap=colour_map, interpolation='nearest')
+    # Create the colour bar
+    scalar_map.set_array(input_array)
+    cbar = plt.colorbar(scalar_map, aspect=20)
+    plt.show()
 
 
 def flatten(input_list):
@@ -113,13 +215,47 @@ if __name__ == "__main__":
         ),
     )
     parser.add_argument(
-        "-m",
-        "--mesh",
+        "-ms",
+        "--mesh_spacing",
         type=float,
         required=False,
         default=10.0,
         help=(
             "The spacing between mesh probes (grid resolution) in angstroems"
+        ),
+    )
+    parser.add_argument(
+        "-mt",
+        "--mesh_type",
+        type=str,
+        required=False,
+        default="C",
+        help=(
+            "The atom type to use for the probe atoms in the mesh. Required for"
+            " calculating the potential energy at each probe site."
+        ),
+    )
+    parser.add_argument(
+        "-r",
+        "--r_cut",
+        type=float,
+        required=False,
+        default=10.0,
+        help=(
+            "The pair interaction cut-off in angstroems"
+        ),
+    )
+    parser.add_argument(
+        "-u",
+        "--u_max",
+        type=float,
+        required=False,
+        default=None,
+        help=(
+            "The maximum potential to consider, in energy units. If the probe"
+            " potential is more than this value, it will be truncated to this value."
+            " The force is expected to be sufficiently high at and beyond this value"
+            " that a particle could not reside there for any length of time."
         ),
     )
     args, directory_list = parser.parse_known_args()
@@ -131,6 +267,20 @@ if __name__ == "__main__":
         if (args.job is not None) and (job.get_id() != args.job):
             continue
         job_frame = get_job_frame(job)
-        crystal_posns = get_surface_atoms(job, job_frame)
-        mesh_posns = create_mesh(job_frame, args.mesh, args.z_val)
+        crystal_posns, crystal_types = get_surface_atoms(job, job_frame)
+        mesh_posns, mesh_types, mesh_shape = create_mesh(job_frame, args)
+        crystal_mesh_posns = np.vstack([mesh_posns, crystal_posns])
+        crystal_mesh_types = np.hstack([mesh_types, crystal_types])
+        nlist = create_freud_nlist(
+            job_frame,
+            crystal_mesh_posns,
+            len(mesh_posns),
+            args.r_cut
+        )
+        potential_dict = calculate_potentials(
+            job, nlist, len(mesh_posns), crystal_mesh_posns, crystal_mesh_types,
+            args.u_max,
+        )
+        potential_array = create_potential_array(potential_dict, mesh_shape, args)
+        plot_heatmap(potential_array)
     plt.close()
