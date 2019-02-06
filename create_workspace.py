3c20e267e2dae4fe7c8f846c3713814097a187f8
Matty Jones <mattyjones@boisestate.edu>
2019-02-06 13:47:51 -0700

    Added PYTHONUNBUFFERED to the fry environment

--- create_workspace.py
+++ create_workspace.py
@@ -2,22 +2,23 @@ import copy
 import sys
 import signac
 import itertools
+import numpy as np
 from collections import OrderedDict
 
 
 def get_gen_parameters():
     parameters = OrderedDict()
     # Generate Parameters
-    parameters["dimensions"] = ["30x15x1"]
-    parameters["template"] = ["corundum.pdb"]
-    parameters["crystal_separation"] = [40.0]
+    parameters["dimensions"] = ["16x16x1"]
+    parameters["template"] = ["Ag_surface.pdb"]
+    # parameters["crystal_separation"] = [40.0]
     parameters["reactant_composition"] = ["{'Ag_5nm':1}"]
     parameters["reactant_num_mol"] = [2]
-    parameters["reactant_position"] = ["[[-2.8, 0.0, 5.8], [-2.8, 0.0, 5.8]]"]
-    parameters["forcefield"] = ["['corundum.eam.fs', 'FF_Ag_Sapphire.xml']"]
-    parameters["crystal_x"] = ["0.4759"]
-    parameters["crystal_y"] = ["0.4759"]
-    parameters["crystal_z"] = ["1.299"]
+    parameters["reactant_position"] = ["[[-2.8, 0.0, 4.3], [2.8, 0.0, 4.3]]"]
+    parameters["forcefield"] = ["['Ag.eam.fs']"]
+    parameters["crystal_x"] = ["0.7810"]
+    parameters["crystal_y"] = ["0.7810"]
+    parameters["crystal_z"] = ["0.6440"]
     parameters["job_type"] = ["parent"]
     return list(parameters.keys()), list(itertools.product(*parameters.values()))
 
@@ -25,16 +26,17 @@ def get_gen_parameters():
 def get_sim_parameters():
     parameters = OrderedDict()
     # Simulate Parameters
-    parameters["temperature"] = [1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2600]
-    parameters["run_time"] = [1E7]
+    parameters["temperature"] = [1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350]
+    parameters["run_time"] = [1E8]
     parameters["omit_lj"] = ["Ag-Ag"]
+    parameters["tau"] = [5, 10, 15, 20]
     parameters["energy_scale_unit"] = ["23.06"]
     parameters["job_type"] = ["child"]
     return list(parameters.keys()), list(itertools.product(*parameters.values()))
 
 
 if __name__ == "__main__":
-    project = signac.init_project("AgNP_Corundum_TempSweep")
+    project = signac.init_project("PureAg_Sintering_1E8_DiffTau")
     gen_param_names, gen_param_combinations = get_gen_parameters()
     sim_param_names, sim_param_combinations = get_sim_parameters()
     # Create the generate jobs
